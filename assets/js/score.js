0038a214cb24cf424c8b65ded6f07036cda14bc5
Ell2Z <linujoy917@gmail.com>
2022-05-29 22:19:37 +0800

    Add files via upload

---
+++ assets/js/score.js
@@ -0,0 +1,11 @@
+(function() {
+  "use strict"; // Start of use strict
+
+  var check = document.querySelector('#check');
+  var score = document.querySelector('#score');
+  score._hide;
+  if (!('addEventListener' in check))
+					return;
+
+
+}
